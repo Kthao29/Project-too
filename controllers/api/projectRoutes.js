62cbd1258863640cbf400f88f2bdfbeefdcf0a04
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2023-12-14 19:41:11 -0600

    project routes built for post put and delete

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -8,11 +8,31 @@ const withAuth = require('../../utils/auth')
 
 
 // Controller to create a new project
-router.post('/', withAuth, async (req, res))
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newProject = await Project.create({
+            ...req.body, 
+            user_id: req.session.user_id
+        });
 
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 //Controller to update an existing project
-router.put('/:id', withAuth, async (req, res))
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        await Project.update(req.body, {
+            where: {id: req.params.id}
+        });
+
+        res.status(200).json('Post Updated');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 // Controller to delete an existing project
