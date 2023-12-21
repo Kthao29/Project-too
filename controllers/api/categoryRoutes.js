541676857d8ae0e13042a1930786a837a765cda3
Keng <kengthao57@gmail.com>
2023-12-20 20:43:47 -0600

    added a model for Category.js and have it exported. Also added an EventListener for when browse in the navbar is click, it will take the user to the categories web. Lastly, added category.handlebars and set up the ui of categories.

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -1,37 +1,35 @@
-const router = require('express').Router();
-const { Category, Project } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const router = require('express').Router();
+// const { Category, Project } = require('../../models');
+// const withAuth = require('../../utils/auth');
 
-router.get('/category', withAuth, async (req, res) => {
-    const { category } = req.params.name;
+// router.get('/category', withAuth, async (req, res) => {
+//     const { category } = req.params.name;
   
-    try {
-      const categoryData = await Category.findOne({
-        where: { name: category },
-      });
+//     try {
+//       const categoryData = await Category.findOne({
+//         where: { name: category },
+//       });
   
-      // Extract the category name
-      const categoryName = categoryData ? categoryData.name : 'Default Category';
+
+//       const categoryName = categoryData ? categoryData.name : 'Default Category';
   
-      // Retrieve projects for the category (you may need to adjust this part)
-      const projects = await Project.findAll({
-        include: [
-          {
-            model: Category,
-            where: { name: category },
-          },
-        ],
-      });
+//       const projects = await Project.findAll({
+//         include: [
+//           {
+//             model: Category,
+//             where: { name: category },
+//           },
+//         ],
+//       });
   
-      // Render the 'category' template and pass the category name
-      res.render('category', {
-        projects,
-        category: categoryName,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+//       res.render('category', {
+//         projects,
+//         category: categoryName,
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
 
-module.exports = router;
+// module.exports = router;
